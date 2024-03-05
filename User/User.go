51a9c7cd2627fc2416ff75960760a1c317adb41f
archibaker <yexotortuesprin@gmail.com>
2024-03-05 14:12:09 +0000

    Base commit before level management

---
+++ User/User.go
@@ -0,0 +1,25 @@
+package User
+
+import (
+  "fmt"
+  
+  "bakery/Application"
+)
+
+func User() {
+
+  jsonTest := `[{
+    "IP": "127.0.0.0",
+    "NS": "localhost.com",
+    "LVL": 1
+  },
+  {
+    "IP": "127.0.0.0",
+    "NS": "localhost.com",
+    "LVL": 1
+  }]`
+
+  fmt.Println("[i] User connected")
+   
+  Application.Application(jsonTest)
+}
